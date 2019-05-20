a460fd2e97df05921bb783d5c03cfbc9bc5f3bfa
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-21 00:26:08 +0530

    Routes: retrieve post by slug

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  model: function(params){
+    return RSVP.hash({
+      posts: this.get('store').findAll('post');
+    });
+  },
+  serialize: function(model, params) {
+    return { post_slug: model.get('slug') };
+  }
 });
