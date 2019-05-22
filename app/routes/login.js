dfa4fd2ded28bc74331d56284674daecef81c79f
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-22 20:20:46 +0530

    Route: setup previousPath property in index route and make transition accordingly

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,11 +3,16 @@ import { inject as service } from "@ember/service";
 
 export default Route.extend({
   session: service(),
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    $(window).scrollTop(0);
+    controller.set('previousPath', this._router.currentPath);
+  },
   actions: {
     login(email, password) {
       this.get("session")
         .authenticate("authenticator:oauth2", email, password)
-        .then(() => this.transitionTo("dashboard"))
+        .then(() => this.transitionTo(this.get('previousPath')))
         .catch(() => {
           this.controller.set("errorMessage", "Invalid login.");
         });
