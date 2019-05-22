9353bacd45eba94803ef32272b25186bae0a8cb6
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-22 17:22:44 +0530

    PostSerializer: added function to serialize question_id and tag_list attributes in post model

--- app/serializers/post.js
+++ app/serializers/post.js
@@ -2,4 +2,22 @@ import DS from 'ember-data';
 import { underscore } from '@ember/string';
 
 export default DS.JSONAPISerializer.extend({
+  serialize() {
+    let json = this._super(...arguments);
+    let attributes = json.data.attributes;
+
+    if( attributes["question-id"] ){
+      attributes["question_id"] = attributes["question-id"];
+    }
+
+    if( attributes["tag-list"] ){
+      attributes["tag_list"] = attributes["tag-list"];
+    }
+
+    delete attributes["tag-list"];
+    delete attributes["question-id"];
+
+    json.data.attributes = attributes;
+    return json;
+  }
 });
