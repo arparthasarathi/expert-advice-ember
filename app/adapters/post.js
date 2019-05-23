9cce854ede85ade57888286a34d6cbd28d0bf04f
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-24 00:10:54 +0530

    PostAdapter: added custom endpoints to query the API

--- app/adapters/post.js
+++ app/adapters/post.js
@@ -13,5 +13,13 @@ export default ApplicationAdapter.extend({
     let post = this.get('store').peekRecord('post', id);
     let slug = post.get('slug');
     return `${this.host}/${this.namespace}/posts/` + slug;
+  },
+  urlForQuery(params){
+    if(params.query){
+      return `${this.host}/${this.namespace}/posts/search`;
+    }
+    else {
+      return `${this.host}/${this.namespace}/posts`;
+    }
   }
 });
