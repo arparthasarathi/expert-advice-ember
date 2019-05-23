1dda788a45cc4049247b2cf3b1e511e164d07baf
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-23 21:22:30 +0530

    PostAdapter: added url for update and delete record

--- app/adapters/post.js
+++ app/adapters/post.js
@@ -3,5 +3,15 @@ import ApplicationAdapter from "./application";
 export default ApplicationAdapter.extend({
   urlForFindRecord(slug) {
     return `${this.host}/${this.namespace}/posts/` + slug;
+  },
+  urlForDeleteRecord(id){
+    let post = this.get('store').peekRecord('post', id);
+    let slug = post.get('slug');
+    return `${this.host}/${this.namespace}/posts/` + slug;
+  },
+  urlForUpdateRecord(id){
+    let post = this.get('store').peekRecord('post', id);
+    let slug = post.get('slug');
+    return `${this.host}/${this.namespace}/posts/` + slug;
   }
 });
