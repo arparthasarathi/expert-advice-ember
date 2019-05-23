3f3bac3472ccec9203feed9bf5ac67045198c82b
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-24 00:35:27 +0530

    IndexController: the changes are
    - Added action to filter posts based on tags
    - Sort and return the questions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,24 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { filterBy } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+import { sort } from '@ember/object/computed';
 
 export default Controller.extend({
-  questions: filterBy('model.posts', 'question_id', null)
+  infinity: service(),
+  questions: filterBy('model.posts', 'question_id', null),
+  sortedQuestions: sort('questions', 'sortDefinition'),
+  sortDefinition: computed(function() {
+    return ['created_at:desc']
+  }),
+  actions: {
+    async searchPosts(query) {
+      let posts = await this.infinity.model('post', {
+        query: query,
+        totalPagesParam: 'meta.total',
+        countParam: 'meta.totalRecords'
+      });
+      this.set('model.posts', posts);
+    }
+  }
 });
