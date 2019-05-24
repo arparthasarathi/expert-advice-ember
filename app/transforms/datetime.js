2c3c05f8bc84800473a9705614dd77d837729c25
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-25 00:16:27 +0530

    Transform: generated test stub for the datetime transform

---
+++ app/transforms/datetime.js
@@ -0,0 +1,19 @@
+import DS from 'ember-data';
+import moment from 'moment';
+
+export default DS.Transform.extend({
+  deserialize(serialized) {
+    if (serialized) {
+      return new Date(serialized);
+    } else {
+      return null;
+    }
+  },
+  serialize(deserialized) {
+    if (deserialized) {
+      return moment(deserialized).format();
+    } else {
+      return null;
+    }
+  }
+});
