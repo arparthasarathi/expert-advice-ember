93d9535b4a7d1016450d251d37789a41815005ef
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-25 04:41:29 +0530

    AskRoute: added function to setup the controller

--- app/routes/ask.js
+++ app/routes/ask.js
@@ -3,6 +3,10 @@ import RSVP from 'rsvp';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('previousPath', this._router.currentPath);
+  },
   model: function(){
     return RSVP.hash({
       post: this.store.createRecord('post')
