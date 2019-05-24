2cefc30ef100ac8f0f07dcf3340f4710b1aec57c
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-24 19:44:00 +0530

    Controller: following changes are made
    - Added show controller to sort the answers
    - Generated test case stub for the show controller

---
+++ app/controllers/show.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import { sort } from '@ember/object/computed';
+import { computed } from '@ember/object';
+
+export default Controller.extend({
+  sortedAnswers: sort('model.post.answers', 'sortDefinition'),
+  sortDefinition: computed(function() {
+    return ['created_at:desc']
+  }),
+});
